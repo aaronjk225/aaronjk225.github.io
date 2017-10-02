421cfaa133231339a750692e004f47ee5d575abe
aaronjk225 <aaronk@Aarons-MacBook-Air-2.local>
2017-10-02 19:57:28 -0400

    Initial commit

---
+++ header.php
@@ -0,0 +1,20 @@
+<? php 
+echo '
+<div id="header">
+	<div id="header_content">
+		<a href="index.html"><img id="logo" src="img/Alogo2.svg" height=25></a>
+		<ul>
+			<li><a href="index.html">Home</a></li>
+			<li><a href="aboutme.html">About Me</a></li>
+			<li><a href="programming.html">Programming</a></li>
+			<li><a href="music.html">Music</a></li>
+			<li><a href="3dcad.html">3D/CAD</a></li>
+			<li><a href="hobby.html">Hobby</a></li>
+			<li><a href="resume.html">Resume</a></li>
+			<li><a href="notes.html">Notes</a></li>
+			<li><a href="misc.html">Misc.</a></li>
+		</ul>
+	</div>
+</div>
+';
+?>
